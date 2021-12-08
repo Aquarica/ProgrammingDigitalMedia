3fa305bdb78ead6607c569983419c94679dc8aa2
Aquarica <erica_winfrey@yahoo.com>
2021-12-08 06:06:23 -0500

    go

--- Sketches/1/sketch.js
+++ Sketches/1/sketch.js
@@ -1,273 +1,232 @@
 let grid;
-let cols;
-let rows;
+let cols, rows;
 let resolution = 10;
+let nonRandomizeRules = true;
 let startGame = false;
-let gameState = "Click Cell to Change Color \n Left arrow to start \n Right arrow to pause "
-
-function make2DArray(cols, rows) {
-    let arr = new Array(cols);
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = new Array(rows);
-    }
-    return arr;
-}
-
-
+let infoMenu = false;
+let tipText;
+let menuText = "Press 'i' for info";
+let textColor = 220;
 
 function setup() {
     createCanvas(600, 400);
     cols = width / resolution;
     rows = height / resolution;
-
-    createRandomArray();
+    gridSet = new LifeBoard(cols, rows);
+    gridSet.createRandomArray();
 }
 
 function draw() {
     background(0);
+    gridSet.colorChange();
+    if (startGame) {
+        gridSet.nextGridCompute();
+    }
+    textManage();
+}
 
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-            let x = i * resolution;
-            let y = j * resolution;
-            if (grid[i][j] == 1) {
-                fill(255);
-                stroke(0);
-                rect(x, y, resolution - 1, resolution - 1);
+function textManage() {
+    if (infoMenu) {
+        tipText = " "
+    } else {
+        tipText = "Info... \n i: toggle menu \n p: pause/play \n x: new random board \n c: clear board \n r: toggle rule randomization \n click: change cell color"
+        fill(50)
+        rectMode(CENTER)
+        rect(width / 2, height / 2, 300, 200, 20)
+
+        strokeWeight(3)
+        textSize(20)
+        fill(textColor)
+        textAlign(LEFT, CENTER);
+        text(tipText, width / 2 - 130, height / 2);
+    }
+
+    textSize(14)
+    textAlign(LEFT, TOP);
+    fill(textColor)
+    strokeWeight(3)
+    text(menuText, 5, 5);
+
+    strokeWeight(0.5)
+    rectMode(CORNER)
+}
+
+function mousePressed() {
+    //console.log(grid[floor(mouseX / resolution)][floor(mouseY / resolution)])
+    grid[floor(mouseX / resolution)][floor(mouseY / resolution)] += 1;
+
+    if (grid[floor(mouseX / resolution)][floor(mouseY / resolution)] > 4) {
+        grid[floor(mouseX / resolution)][floor(mouseY / resolution)] = 0;
+    }
+}
+
+//GAME CONTROLS
+function keyPressed() {
+    //starts and pauses game
+    if (keyCode === 80) {
+        startGame = !startGame;
+    }
+
+    // if (keyCode === LEFT_ARROW) {
+    //     if (resolution >= 5) {
+    //         resolution = resolution - 1;
+    //         console.log(resolution);
+    //         cols = width / resolution;
+    //         rows = height / resolution;
+    //         gridSet = new LifeBoard(cols, rows);
+    //         gridSet.createRandomArray();
+    //     }
+    // }
+    // if (keyCode === RIGHT_ARROW) {
+    //     if (resolution <= 20) {
+    //         resolution = resolution + 1;
+    //         console.log(resolution);
+    //     }
+    // }
+
+    //clears the board
+    if (keyCode === UP_ARROW) {
+        gridSet.boardClear()
+    }
+    //makes new random array
+    if (keyCode === DOWN_ARROW) {
+        gridSet.createRandomArray()
+    }
+    //r key, makes it so rules can have some randomization
+    if (keyCode === 82) {
+        nonRandomizeRules = !nonRandomizeRules;
+    }
+    //i key
+    if (keyCode === 73) {
+        infoMenu = !infoMenu;
+        //console.log(infoMenu);
+    }
+}
+
+
+//Manages the Board and it's functions
+class LifeBoard {
+    constructor(cols, rows) {
+        this.cols = cols;
+        this.rows = rows;
+
+    }
+    make2DArray() {
+        let arr = new Array(this.cols);
+        for (let i = 0; i < arr.length; i++) {
+            arr[i] = new Array(this.rows);
+        }
+        return arr;
+    }
+    createRandomArray() {
+        grid = this.make2DArray(this.cols, this.rows);
+        for (let i = 0; i < this.cols; i++) {
+            for (let j = 0; j < this.rows; j++) {
+                let randomNumber = floor(random(10));
+                if (randomNumber > 0) {
+                    grid[i][j] = floor(random(2));
+                } else {
+                    grid[i][j] = floor(random(5));
+                }
             }
         }
     }
 
-    if (startGame) {
-        nextGridCompute();
-        gameState = " "
+    //neighbor count is used to determine life/death of cell in nextGrid Compute
+    countNeighbors(grid, x, y) {
+        let sum = 0;
+        for (let i = -1; i < 2; i++) {
+            for (let j = -1; j < 2; j++) {
+                let col = (x + i + cols) % this.cols;
+                let row = (y + j + rows) % this.rows;
+                sum += grid[col][row];
+            }
+        }
+        sum -= grid[x][y];
+        return sum;
     }
-    fill(50, 200, 100)
-    textSize(30)
-    strokeWeight(4)
-    text(gameState, width / 2, height / 2);
-    textAlign(CENTER);
-    strokeWeight(0.5)
 
-    function nextGridCompute() {
-        let next = make2DArray(cols, rows);
-
+    //this is run every frame the game isn't paused
+    nextGridCompute() {
+        let next = this.make2DArray(this.cols, this.rows);
         // Compute next based on grid
-        for (let i = 0; i < cols; i++) {
-            for (let j = 0; j < rows; j++) {
+        for (let i = 0; i < this.cols; i++) {
+            for (let j = 0; j < this.rows; j++) {
                 let state = grid[i][j];
                 // Count neighbors 
                 let sum = 0;
-                let neighbors = countNeighbors(grid, i, j);
-
-                if (state == 0 && neighbors == 3) {
-                    next[i][j] = 1;
-                } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
-                    next[i][j] = 0;
+                let neighbors = this.countNeighbors(grid, i, j);
+                if (nonRandomizeRules) {
+                    if (state == 0 && neighbors == 3) {
+                        next[i][j] = 4;
+                    } else if (state == 0 && neighbors == 4) {
+                        next[i][j] = 3;
+                    } else if (state == 0 && neighbors == 5) {
+                        next[i][j] = 2;
+                    } else if (state == 0 && neighbors == 6) {
+                        next[i][j] = 1;
+                    }
+                    else if (state >= 1 && (neighbors < 4 || neighbors > 10)) {
+                        next[i][j] = 0;
+                    }
+                    else {
+                        next[i][j] = state;
+                    }
                 } else {
-                    next[i][j] = state;
+                    if (state == 0 && neighbors == 3) {
+                        next[i][j] = floor(random(5));
+                    } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+                        next[i][j] = 0;
+                    }
+                    //COLOR CELLS HAVE 1% CHANCE OF DEATH seems to go against game of life, might remove
+                    else if (state > 0 && random(100) < 1) {
+                        next[i][j] = 0;
+                    }
+                    else {
+                        next[i][j] = state;
+                    }
                 }
             }
         }
 
         grid = next;
     }
-}
+    colorChange() {
+        for (let i = 0; i < this.cols; i++) {
+            for (let j = 0; j < this.rows; j++) {
+                let x = i * resolution;
+                let y = j * resolution;
+                if (grid[i][j] == 1) {
+                    fill(255);
+                    stroke(0);
+                    rect(x, y, resolution - 1, resolution - 1);
+                }
+                if (grid[i][j] == 2) {
+                    fill(255, 0, 0);
+                    stroke(0);
+                    rect(x, y, resolution - 1, resolution - 1);
+                }
+                if (grid[i][j] == 3) {
+                    fill(0, 255, 0);
+                    stroke(0);
+                    rect(x, y, resolution - 1, resolution - 1);
+                }
+                if (grid[i][j] == 4) {
+                    fill(0, 0, 255);
+                    stroke(0);
+                    rect(x, y, resolution - 1, resolution - 1);
+                }
 
-function countNeighbors(grid, x, y) {
-    let sum = 0;
-    for (let i = -1; i < 2; i++) {
-        for (let j = -1; j < 2; j++) {
-            let col = (x + i + cols) % cols;
-            let row = (y + j + rows) % rows;
-            sum += grid[col][row];
+
+            }
         }
     }
-    sum -= grid[x][y];
-    return sum;
-}
 
-function mousePressed() {
-    console.log(grid[floor(mouseX / resolution)][floor(mouseY / resolution)])
-    // if (grid[floor(mouseX / resolution)][floor(mouseY / resolution)] == 1) {
-    //     grid[floor(mouseX / resolution)][floor(mouseY / resolution)] = 0;
-    // }
-    if (grid[floor(mouseX / resolution)][floor(mouseY / resolution)] == 0) {
-        grid[floor(mouseX / resolution)][floor(mouseY / resolution)] += 1;
-    }
-    else {
-        grid[floor(mouseX / resolution)][floor(mouseY / resolution)] = 0;
-    }
-
-
-}
-
-//FOR pausing and starting the game again
-function keyPressed() {
-    if (keyCode === LEFT_ARROW) {
-        startGame = true;
-    }
-    if (keyCode === RIGHT_ARROW) {
-        startGame = false;
-        gameState = "Paused \n Tip: Up arrow clears board, \n down makes new random board"
-    }
-    if (keyCode === UP_ARROW) {
+    boardClear() {
         for (let i = 0; i < cols; i++) {
             for (let j = 0; j < rows; j++) {
                 grid[i][j] = 0;
             }
         }
     }
-    if (keyCode === DOWN_ARROW) {
-        for (let i = 0; i < cols; i++) {
-            for (let j = 0; j < rows; j++) {
-                grid[i][j] = floor(random(2));
-            }
-        }
-    }
 }
-
-function createRandomArray() {
-    grid = make2DArray(cols, rows);
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-            grid[i][j] = floor(random(2));
-        }
-    }
-}
-
-
-//This is the old sketch to put somewhere else later
-// // All the paths
-// let paths = [];
-// // Are we painting?
-// let painting = false;
-// // How long until the next circle
-// let next = 0;
-// // Where are we now and where were we?
-// let current;
-// let previous;
-
-// function setup() {
-//     createCanvas(windowWidth, windowHeight);
-//     current = createVector(0, 0);
-//     previous = createVector(0, 0);
-//     //background = (100,100,100);
-//     frameRate(10);
-// };
-
-// function draw() {
-//     background(200,200,200);
-
-//     // If it's time for a new point
-//     if (millis() > next && painting) {
-
-//         // Grab mouse position      
-//         current.x = mouseX;
-//         current.y = mouseY;
-
-//         // New particle's force is based on mouse movement
-//         let force = p5.Vector.sub(current, previous);
-//         force.mult(0.05);
-
-//         // Add new particle
-//         paths[paths.length - 1].add(current, force);
-
-//         // Schedule next circle
-//         next = millis() + random(100);
-
-//         // Store mouse values
-//         previous.x = current.x;
-//         previous.y = current.y;
-//     }
-
-//     // Draw all paths
-//     for (let i = 0; i < paths.length; i++) {
-//         paths[i].update();
-//         paths[i].display();
-//     }
-// }
-
-// // Start it up
-// function mousePressed() {
-//     next = 0;
-//     painting = true;
-//     previous.x = mouseX;
-//     previous.y = mouseY;
-//     paths.push(new Path());
-// }
-
-// // Stop
-// function mouseReleased() {
-//     painting = false;
-// }
-
-// // A Path is a list of particles
-// class Path {
-//     constructor() {
-//         this.particles = [];
-//         this.hue = random(100);
-//     }
-
-//     add(position, force) {
-//         // Add a new particle with a position, force, and hue
-//         this.particles.push(new Particle(position, force, this.hue));
-//     }
-
-//     // Display plath
-//     update() {
-//         for (let i = 0; i < this.particles.length; i++) {
-//             this.particles[i].update();
-//         }
-//     }
-
-//     // Display plath
-//     display() {
-//         // Loop through backwards
-//         for (let i = this.particles.length - 1; i >= 0; i--) {
-//             // If we shold remove it
-//             if (this.particles[i].lifespan <= 0) {
-//                 this.particles.splice(i, 1);
-//                 // Otherwise, display it
-//             } else {
-//                 this.particles[i].display(this.particles[i + 1]);
-//             }
-//         }
-
-//     }
-// }
-
-// // Particles along the path
-// class Particle {
-//     constructor(position, force, hue) {
-//         this.position = createVector(position.x, position.y);
-//         this.velocity = createVector(force.x*3, force.y*3);//Defines push away speed
-//         this.drag = 1;//How far can it go?
-//         this.lifespan = 255;
-//     }
-
-//     update() {
-//         // Move it
-//         this.position.add(this.velocity);
-//         // Slow it down
-//         this.velocity.mult(this.drag);
-//         // Fade it out
-//         this.lifespan--;
-//     }
-
-//     // Draw particle and connect it with a line
-//     // Draw a line to another
-//     display(other) {
-//         let ranColorR = random(0,255);
-//         let ranColorG = random(0,255);
-//         let ranColorB = random(0,255);
-//         stroke(ranColorR, ranColorG, ranColorB, this.lifespan);
-//         strokeWeight(100);
-//         fill(ranColorR, ranColorG, ranColorB, this.lifespan / 2);
-//         //ellipse(this.position.x, this.position.y, 10, 10);
-//         // If we need to draw a line
-//         if (other) {
-//             line(this.position.x, this.position.y, other.position.x, other.position.y);
-//         }
-//     }
-// }
